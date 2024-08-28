796b5a34a8c5661c1354323f1d4b5f6fccc629d1
Matheus H <matheusurban152@gmail.com>
2024-08-28 11:23:52 -0300

    aula 28/08

--- solucao/API/Program.cs
+++ solucao/API/Program.cs
@@ -1,6 +1,28 @@
+//Testar as APIs
+// - Rest CLient - extensão do vs code
+// - Postman
+// - Insomnia 
+
+//MINIMAL APIs
+using System.Runtime.InteropServices;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//EndPoints - Funcionalidades
+//Configurar a URL e p método/verbo
+app.MapGet("/", () => "Hello World em C#");
+
+app.MapGet("/segundafuncionalidade", () => "sla");
+
+app.MapGet("/retornarendereco", () => 
+{
+    dynamic endereco = new {
+        rua = "Parigot de Souza",
+        numero = 5300,
+        cep = "81290-350"
+    };
+    return endereco;
+});
 
 app.Run();
